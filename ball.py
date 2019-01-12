6e1003a5e218e83bf951d5c0d8b7ae764d1c7958
lz <lzlz0810@qq.com>
2019-01-12 17:59:03 +0800

    '初始提交'

---
+++ ball.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+import time
+
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+fps = 300
+size = width, height =  600, 400
+speed = [1,1]
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("pygame-ball")
+font = pygame.font.Font("Microsoft Yahei Mono.ttf",100)
+ball = font.render("●", True, WHITE)
+# ball = pygame.image.load("ball.png")
+ballRect = ball.get_rect()
+print(ballRect)
+ball_interval = 8, 30
+clock = pygame.time.Clock()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                speed[0] = 0 if speed[0]== 0 else (abs(speed[0])-1)*int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0] + 1 if speed[0]> 0 else speed[0] - 1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1]> 0 else speed[1] - 1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = 0 if speed[1]== 0 else (abs(speed[1])-1)*int(speed[1]/abs(speed[1]))
+    ballRect = ballRect.move(speed)
+    if ballRect.left < -ball_interval[0] or ballRect.right > width+ball_interval[0]:
+        speed[0] = -speed[0]
+    if ballRect.top < -ball_interval[1] or ballRect.bottom > height+ball_interval[1]:
+        speed[1] = -speed[1]
+
+    # 刷新窗口
+    screen.fill(BLACK)
+    screen.blit(ball,ballRect)
+    pygame.display.update()
+    clock.tick(fps)
+
---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("pygame-哈哈哈")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        pygame.display.update()
